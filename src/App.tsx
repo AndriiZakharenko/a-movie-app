2bd8e613ef11e9c82d16bc80a49af1c976ba104b
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-02-08 22:32:28 +0200

    feat-header: header, search

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Search from "./components/Search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+    <main>
+      <div className="pattern" />
 
-export default App
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+      </div>
+    </main>
+  );
+};
+
+export default App;
